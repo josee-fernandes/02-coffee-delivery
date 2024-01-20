638eb94709d6a6b825f5c3719878579d02e16715
José Vitor dos Santos Fernandes <jose-vitor-7@hotmail.com>
2024-01-20 00:27:43 -0300

    Update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,37 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import macrosPlugin from 'vite-plugin-babel-macros'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    manifest: true,
+  },
+  plugins: [
+    react({
+      include: /\.(jsx|tsx)$/,
+      babel: {
+        babelrc: false,
+        configFile: false,
+        presets: ['@babel/preset-typescript'],
+        plugins: [
+          '@babel/plugin-transform-typescript',
+          [
+            'babel-plugin-styled-components',
+            {
+              ssr: false,
+              pure: true,
+              displayName: true,
+              fileName: false,
+            },
+          ],
+        ],
+      },
+    }),
+    macrosPlugin(),
+  ],
   base: '/coffee-delivery/',
 })
